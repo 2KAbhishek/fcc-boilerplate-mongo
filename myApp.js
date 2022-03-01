75f2cb67b079a73a32e2b12505ccc6380fd63567
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-02 03:00:44 +0530

    Chain queries

--- myApp.js
+++ myApp.js
@@ -85,9 +85,11 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
-};
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({age: 0}).exec((err, data) => {
+    if (err) { return done(err); }
+    done(null, data);
+  });
+}
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
