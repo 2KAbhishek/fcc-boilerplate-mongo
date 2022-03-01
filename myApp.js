08266378d48173b9a91e7fb10380f6dd95e19ee2
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-02 01:54:25 +0530

    Find and remove

--- myApp.js
+++ myApp.js
@@ -69,8 +69,11 @@ const findAndUpdate = (personName, done) => {
 }
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
-};
+  Person.findByIdAndRemove(personId, (err, data) => {
+    if (err) { return done(err); }
+    done(null, data);
+  });
+}
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
