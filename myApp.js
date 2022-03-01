6dddfc822374398c022f33966f3ae9ce0b16a57f
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-02 01:46:22 +0530

    Find, edit and save a document

--- myApp.js
+++ myApp.js
@@ -49,8 +49,15 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
+  Person.findById(personId, (err, person) => {
+    if (err) { return done(err); }
+    person.favoriteFoods.push(foodToAdd);
 
-  done(null /*, data*/);
+    person.save((err, updatedPerson) => {
+      if (err) { return done(err); }
+      done(null, updatedPerson);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
