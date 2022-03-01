13e854aef14580f43b78802a5208bc9492f28f4c
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-02 01:53:08 +0530

    Find and update

--- myApp.js
+++ myApp.js
@@ -62,9 +62,11 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
-};
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedPerson) => {
+    if (err) { return done(err); }
+    done(null, updatedPerson);
+  });
+}
 
 const removeById = (personId, done) => {
   done(null /*, data*/);
